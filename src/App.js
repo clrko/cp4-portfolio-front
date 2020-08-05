2324ff08018427fc0c9d8d6fc7fdfbdf55aa1de1
Claire Kodia <claire.kodia@gmail.com>
2020-08-05 18:20:23 +0200

    Set up react router

--- src/App.js
+++ src/App.js
@@ -1,8 +1,12 @@
 import React from 'react'
+import { Route, Switch } from 'react-router-dom'
+import LandingPage from './components/pages/LandingPage'
 
 const App = () => {
   return (
-    <div>Hello</div>
+    <Switch>
+      <Route exact path='/' component={LandingPage} />
+    </Switch>
   )
 }
 
---
+++ src/components/pages/LandingPage.js
@@ -0,0 +1,14 @@
+import React from 'react'
+
+const LandingPage = () => {
+  return (
+    <div>
+      <div>Header</div>
+      <div>About me</div>
+      <div>ListProjet</div>
+      <div>Footer</div>
+    </div>
+  )
+}
+
+export default LandingPage
